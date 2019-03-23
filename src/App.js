3b068efa3579f793d238c7e2731e63e669e8db65
David Hajos <dh.it.email@gmail.com>
2019-03-23 13:17:54 +0000

    3 # Add Data, Begin with Componenet state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author : 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {        
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author : 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+];
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+    };
+  }
   render() {
-    const helloWord = 'Welcome to the Road to learn React.';
-    let user = {firstName: 'David', lastName: 'H'};
-    let intro = 'This is a test application'
     return (
       <div className="App">
-          <h2>
-            {helloWord}
-          </h2>
-          <h3>
-            {intro} that I am buiding to better my React and JS skills.
-            Oh... by the way my name is {user.firstName}
-          </h3>
+        {this.state.list.map(item => 
+          <div key = {item.objectID}>
+            <span>
+              <a herf={item.url} >{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div> 
+        )}
       </div>
     );
   }
