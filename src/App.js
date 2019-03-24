fa87ad59b34616ec68bdc299b814b2c9cd354a79
David Hajos <dh.it.email@gmail.com>
2019-03-24 02:02:56 +0000

    9 # Styling components

--- src/App.js
+++ src/App.js
@@ -48,66 +48,76 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          Search
-        </Search>
-        <Table
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
+          <Table
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
       </div>
       );
     }
   }
 export default App;
 
-function Search( { value, onChange, children }) {
-    return (
-      <form>
-        {children} <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-  } 
+const Search = ( { value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
-function Table({ list, pattern, onDismiss }) {
-  return (
-    <div>  
-      {list.filter(isSearched(pattern)).map(item =>
-      <div key={item.objectID}>
-        <span>
-          <a herf={item.url} >{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-          <button onClick={() => onDismiss(item.objectID)}>
-            Dismiss
-          </button>
-        </span>
-      </div> 
-      )}
-    </div>
-  );
-}
+const Table = ({ list, pattern, onDismiss }) =>
+  <div className="table">  
+    {list.filter(isSearched(pattern)).map(item =>
+    <div key={item.objectID} className="table-row">
+      <span style = {largeColumn}>
+        <a herf={item.url} >{item.title}</a>
+      </span>
+      <span style = {medColumn}>
+        {item.author}</span>
+      <span style = {smallColumn}>
+        {item.num_comments}</span>
+      <span style = {smallColumn}>
+        {item.points}</span>
+      <span style = {smallColumn}>
+      <Button onClick={() => onDismiss(item.objectID)}
+        className="button-inline"
+        >
+          Dismiss
+        </Button>
+      </span>
+    </div> 
+    )}
+  </div>
 
-function Button({ onClick, className = '', children }) {
-  return (
-    <button
-      onClick={onClick}
-      className={className}
-      type="button"
-    >
-      {children}
-    </button>
-  );
-}
+const Button = ({ onClick, className = '', children }) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
+
+const largeColumn = {
+  width: '40%',
+};
+
+const medColumn = {
+  width: '30%',
+};
+
+const smallColumn = {
+  width: '10%',
+};
